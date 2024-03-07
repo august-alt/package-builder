ad4d2a67a61e40a165c518097900b50accce927a
august-alt <77973983+august-alt@users.noreply.github.com>
2024-03-07 13:22:33 +0400

    feat: add debian.py file

---
+++ tools/debian.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import urllib.request
+
+class DebianPackage(object):
+    def __init__(self, path):
+        self.build_path = None
+        self.package_path = path
+
+        self.package_url = None
+        self.tar_archive = None
+        self.archive_dir = None
+
+        self.read_package_url()
+        self.read_changelog()
+
+    def read_package_url(self):
+        url_file = os.path.join(self.package_path, "package_url")
+
+        if not os.exists(url_file):
+            print(f"Unable to find file {file}", url_file)
+        else:
+            with open(url_file) as f:
+                self.package_url = f.read().trim()
+                close(url_file)
+
+    def read_changelog(self):
+        pass
+
+    def build_package(self):
+        if (not self.tar_archive):
+            return False
+
+        self.__create_build_dir()
+        if self.package_url:
+            self.__fetch_tarball()
+            self.__extract_tarball()
+        self.__copy_debian()
+        return self.__build()
+
+    def __create_build_dir(self):
+        if os.exists(self.build_path):
+            return
+
+        os.mkdir(self.build_path)
+
+    def __fetch_tarball(self):
+        pass
+
+    def __extract_tarball(self):
+        pass
+
+    def __copy_debian(self):
+        pass
+
+    def __build(self):
+        pass
+
+def main(self):
+    if sys.argc < 2:
+        print("Usage: {1} <package_path>".format(sys.argv[0]))
+        return
+
+    path = sys.argv[1]
+
+    if not os.path.exists(path):
+        print("Given path {1} is invalid!".format(path))
+
+    if not os.path.exists(os.path.join(path, "debian")):
+        print("There is no debian folder in given path!")
+        return
+
+    package = DebianPackage(path)
+    if not package.build_package():
+        print("Error building package!")
+
